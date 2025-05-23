ac3b3226681d3a637077b260ddbe852159a24437
illyadmytrenko <illadmitrenko666@gmail.com>
2025-05-23 21:22:41 +0300

    updated UX

--- src/app/components/blob/blob-portfolio.tsx
+++ src/app/components/blob/blob-portfolio.tsx
@@ -16,38 +16,35 @@ export function BlobPortfolio({ blob }: BlobProps) {
 
   return (
     <div key={blob.src}>
-      <div
-        className="relative w-fit !z-20 overflow-hidden hover:scale-110 transition-transform"
-        onMouseEnter={() => handleOnHover(true)}
-        onMouseLeave={() => handleOnHover(false)}
-      >
-        <img
-          src={blob.src}
-          alt="portfolio icon"
-          width={300}
-          height={300}
-          className="cursor-pointer"
-        />
-        <p className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 w-full flex justify-center">
-          {blob.text}
-        </p>
-      </div>
-      {isWebsiteShowed && (
-        <Link
-          href={blob.websiteSrc ?? ""}
+      <Link href={blob.websiteSrc ?? ""} target="_blank">
+        <div
+          className="relative w-fit !z-20 overflow-hidden hover:scale-110 transition-transform"
           onMouseEnter={() => handleOnHover(true)}
           onMouseLeave={() => handleOnHover(false)}
-          target="_blank"
         >
           <img
+            src={blob.src}
+            alt="portfolio icon"
+            width={300}
+            height={300}
+            className="cursor-pointer"
+          />
+          <p className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 w-full flex justify-center">
+            {blob.text}
+          </p>
+        </div>
+        {isWebsiteShowed && (
+          <img
+            onMouseEnter={() => handleOnHover(true)}
+            onMouseLeave={() => handleOnHover(false)}
             src={blob.websiteImgSrc ?? ""}
             alt="portfolio icon"
             width={500}
             height={500}
             className="relative -bottom-5 sm:-top-20 z-10 sm:left-[15%] border-2 border-solid border-green-600 rounded-md"
           />
-        </Link>
-      )}
+        )}
+      </Link>
     </div>
   );
 }
